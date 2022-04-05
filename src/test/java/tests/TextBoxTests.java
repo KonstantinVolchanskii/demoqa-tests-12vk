6839ccbe0ad34030b3d87baff8c2d378981bad5b
KonstantinVolchanskii <konstantingrandf@gmail.com>
2022-04-05 12:33:44 +0300

    added tests

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -41,9 +41,38 @@ public class TextBoxTests {
         String month = birth.getMonth().toString().charAt(0)                 //получаем из даты название месяца
                 + birth.getMonth().toString().substring(1).toLowerCase();
         SelenideElement stateCity = $("#stateCity-wrapper");
-        ArrayList<SelenideElement> actions = new ArrayList<>();              
+        ArrayList<SelenideElement> actions = new ArrayList<>();
         actions.add(stateCity.$(byText("Select State")));
         actions.add(stateCity.$(byText(state)));
         actions.add(stateCity.$(byText("Select City")));
         actions.add(stateCity.$(byText(city)));
+        
+        open("/automation-practice-form");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#genterWrapper").$(byText(gender)).click();
+        $("#userNumber").setValue(phoneNumber);
+        //$(byXpath("//*[@id='submit']")).scrollIntoView(true);       //скролл на случай маленького экрана/рекламы
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(String.valueOf(birth.getYear()));
+        $("[aria-label$='" + month + " " + birth.getDayOfMonth() + "th, " + birth.getYear() + "']").click();
+        //$(byXpath("//div[@class='react-datepicker__day react-datepicker__day--0" + birth.getDayOfMonth() + "']")).click();        //альтернатива через Xpath
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $("#hobbiesWrapper").$(byText(hobby)).click();
+        $("#uploadPicture").uploadFromClasspath(imgPath);
+        $("#currentAddress").setValue(address);
+        for (SelenideElement selenideElement : actions) {
+            selenideElement.click();                                //прокликиваем всю коллекцию
+        }
+        $("#submit").click();
 
+        //Блок проверок
+        //$("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        //$(".table-responsive").shouldHave(text(firstName + " " + lastName), text(email), text(gender),
+        //        text(phoneNumber), text(birth.getDayOfMonth() + " " + month + "," + birth.getYear()), text(subject),
+        //text(hobby), text(imgPath.substring(4)), text(address), text(state + " " + city));
+        //$("#closeLargeModal").click();
+    }
+}
